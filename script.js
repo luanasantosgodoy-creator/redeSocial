b1fd6fbc66283e0ac86131a4e8d07bf0c955981a
luanasantosgodoy-creator <luana.santos.godoy@escola.pr.gov.br>
2026-08-06 12:14:27 -0300

    ok

---
+++ script.js
@@ -0,0 +1,7 @@
+//Botão curtida
+
+document.addEventListener("DOMContentLoaded", () => {
+    const likeBtn = document.querySelector(".left-actions .adtion-btn:first-child");
+    if(!likeBtn) return;
+    const likeSvg = likeBtn. querySelector("svg");
+})
