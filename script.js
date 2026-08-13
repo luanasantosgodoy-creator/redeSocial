dac0948e42293cac83d4615d9f748d9cb84fe659
luanasantosgodoy-creator <luana.santos.godoy@escola.pr.gov.br>
2026-08-13 12:16:14 -0300

    ok

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const likeBtn = document.querySelector(".left-actions .adtion-btn:first-child");
     if(!likeBtn) return;
     const likeSvg = likeBtn. querySelector("svg");
+
+    //localiza o contador
+
+    let textNode = Array.from(likeBtn.childNodes).find(node) => node.nodeType
 })
